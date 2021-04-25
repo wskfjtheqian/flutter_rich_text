3fabc41c77b113eeaa925d9710262ed0aaf1f403
heqian <182359041@qq.com>
2021-04-25 14:22:26 +0800

    dd

---
+++ ss/flutter_rich_text/lib/rich_text_edit.dart
@@ -0,0 +1,5 @@
+library rich_text_edit;
+
+export 'src/rich_editable.dart';
+export 'src/rich_span.dart';
+export 'src/rich_edit_field.dart';
---
+++ ss/flutter_rich_text/lib/src/rich_span.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/cupertino.dart';
+import 'dart:ui' as ui show PlaceholderAlignment;
+
+class RichSpan extends WidgetSpan {
+  static final codeTextStart = '\uE000';
+  static final codeTextEnd = '\uF8FF';
+
+  final String codeText;
+
+  const RichSpan(
+    this.codeText, {
+    child,
+    ui.PlaceholderAlignment alignment = ui.PlaceholderAlignment.bottom,
+    TextBaseline? baseline,
+    TextStyle? style,
+  })  : assert(child != codeText),
+        assert(child != null),
+        assert(baseline != null ||
+            !(identical(alignment, ui.PlaceholderAlignment.aboveBaseline) ||
+                identical(alignment, ui.PlaceholderAlignment.belowBaseline) ||
+                identical(alignment, ui.PlaceholderAlignment.baseline))),
+        super(
+          child: child,
+          alignment: alignment,
+          baseline: baseline,
+          style: style,
+        );
+
+  @override
+  int? codeUnitAtVisitor(int index, Accumulator offset) {
+    if (index - offset.value < codeText.length) {
+      return codeText.codeUnitAt(0);
+    }
+    offset.increment(codeText.length);
+    return null;
+  }
+}
