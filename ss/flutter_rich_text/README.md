0b0119ce2fe6a7760e9afbf0067d9ba69ffbbe2b
heqian <182359041@qq.com>
2021-04-25 14:24:07 +0800

    删除 不用的

--- ss/flutter_rich_text/README.md
+++
@@ -1,7 +0,0 @@
-# rich_text_edit
-
-A new Flutter package project.
-
-## Getting Started
-
-Flutter Sdk <= 1.25.0
--- ss/flutter_rich_text/lib/rich_text_edit.dart
+++
@@ -1,5 +0,0 @@
-library rich_text_edit;
-
-export 'src/rich_editable.dart';
-export 'src/rich_span.dart';
-export 'src/rich_edit_field.dart';
